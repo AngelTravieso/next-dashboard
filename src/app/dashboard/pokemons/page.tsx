366fbe788a4be862208d9291b7b268aa5f6c6b09
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-03 17:01:16 -0400

    Data Fetching - Next13+

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,13 @@
-export default function PokemonPage() {
-  return (
-    <div>
-      <h1>Pokemon Page</h1>
-    </div>
-  );
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&ofsset=${offset}`
+  ).then((value) => value.json());
+
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
+  return <div>{JSON.stringify(pokemons)}</div>;
 }
