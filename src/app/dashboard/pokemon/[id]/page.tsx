93ff224888e8dc605f8eb7b9e6ed8a3ce5f853a9
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-04 19:23:48 -0400

    Cargar informacion del pokemon por ID

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,8 +1,22 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
   params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache", // TODO cambiar esto en un futuro
+  }).then((resp) => resp.json());
+
+  console.log('Se cargó:', pokemon.name);
+
+  return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
   /**
    * Esto muestra el pathVariable y queryParams
    * { params: { id: '1' }, searchParams: { query: 'char', age: '31' } }
@@ -13,6 +27,11 @@ export default function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Pokémon {params.id}</h1>
+      <div>
+        {
+            JSON.stringify(pokemon)
+        }
+      </div>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -2,3 +2,4 @@ export { PokemonGrid } from './components/PokemonGrid';
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
