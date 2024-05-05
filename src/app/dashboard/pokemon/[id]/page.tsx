878f68ac39e35009ce54bc6c831a112dcb0e0a7f
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-05 17:33:39 -0400

    Metadata dinamica

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,20 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
+}
+
+
+// Metadata dinámica
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+
+  const { id, name } = await getPokemon(params.id);
+
+  return {
+    title: `#${ id } - ${ name }`,
+    description: `Página del pokémon ${ name }`,
+  }
 }
 
 const getPokemon = async (id: string): Promise<Pokemon> => {
@@ -29,7 +42,7 @@ export default async function PokemonPage({ params }: Props) {
       <h1>Pokémon {params.id}</h1>
       <div>
         {
-            JSON.stringify(pokemon)
+            pokemon.name
         }
       </div>
     </div>
