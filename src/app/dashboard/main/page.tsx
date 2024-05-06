46bcef705ac123907339ed577ecf4079363fdcf4
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-06 16:41:10 -0400

    Instalacion y configuracion de Redux Toolkit

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -2,19 +2,18 @@
 
 import { SimpleWidget } from "@/components";
 
+export const metadata = {
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
+
 export default function MainPage() {
   return (
     <div className="text-black p-2">
       <h1 className="mt-2 text-3xl">Dashboard</h1>
       <span className="text-xl">Información general</span>
 
-      <div className="flex flex-wrap p-2">
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
+      <div className="flex flex-wrap p-2 items-center justify-center">
         <SimpleWidget />
       </div>
     </div>
