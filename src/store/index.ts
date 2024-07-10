565d0e9fd828ef311c21f83c227731204e80a930
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-09 21:56:08 -0400

    Exportando redux toolkit hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from './counter/counterSlice';
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -12,3 +13,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
